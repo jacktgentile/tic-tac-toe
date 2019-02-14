9f83401a57f98fe7c8b58a66c7a2a71ff76ad0c2
jacktgentile <32143117+jacktgentile@users.noreply.github.com>
2019-02-13 22:21:14 -0600

    added visual.py to create display for testing pentomino

--- README.md
+++ README.md
@@ -6,8 +6,9 @@ Artificial intelligence that uses minimax and alpha-beta pruning strategies in g
 
 Solves a [pentomino](https://en.wikipedia.org/wiki/Pentomino) game, and accomplishes the same with dominos and trionomos.
 Relevant files are [solve.py](solve.py), [Pentomino.py](Pentomino.py), and [instances.py](instances.py).
+Can be run using the command "python visual.py" which launches a window displaying the solved layout.
 
 ## Ultimate Tic-Tac-Toe
 
-Uses artificial intelligence techniques to play a game of ultimate tic-tac-toe. 
+Uses artificial intelligence techniques to play a game of ultimate tic-tac-toe.
 Relevant file is [uttt.py](uttt.py).
--- solve.py
+++ solve.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
 
+"""
+This is the function you will implement. It will take in a numpy array of the board
+as well as a list of n tiles in the form of numpy arrays. The solution returned
+is of the form [(p1, (row1, col1))...(pn,  (rown, coln))]
+where pi is a tile (may be rotated or flipped), and (rowi, coli) is
+the coordinate of the upper left corner of pi in the board (lowest row and column index
+that the tile covers).
 
-def solve(board, pents):
-    """
-    This is the function you will implement. It will take in a numpy array of the board
-    as well as a list of n tiles in the form of numpy arrays. The solution returned
-    is of the form [(p1, (row1, col1))...(pn,  (rown, coln))]
-    where pi is a tile (may be rotated or flipped), and (rowi, coli) is 
-    the coordinate of the upper left corner of pi in the board (lowest row and column index 
-    that the tile covers).
-    
-    -Use np.flip and np.rot90 to manipulate pentominos.
-    
-    -You can assume there will always be a solution.
-    """
-    
+-Use np.flip and np.rot90 to manipulate pentominos.
+
+-You can assume there will always be a solution.
+"""
+
+def solve(board, pents, app=None):
+    my_sol = []
+    if app is not None:
+        app.draw_solution_and_sleep(my_sol, 1)
+    return my_sol
+
     raise NotImplementedError
